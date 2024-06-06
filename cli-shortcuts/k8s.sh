b47be784b190c2875484b8ace3ccf83b84b708fc
Gagan Talreja <gagan.talreja@schrodinger.com>
2024-06-06 12:55:56 +0530

    removing unused aliases

--- cli-shortcuts/k8s.sh
+++ cli-shortcuts/k8s.sh
@@ -100,44 +100,6 @@ function kxr() {
 	done;
 }
 
-
-# get pod identifier
-# usage : pod <namespace> <service-name>
-function pod() {
-        cands=`kubectl -n $1 get pods| grep -v 'No resources found' | grep Running | awk '{print $1}'`
-        (echo "$cands" | grep ^$2-$1) ||  (echo "$cands" | grep ^$2) ||  (echo "$cands" | grep ^$1) ||  (echo "$cands" | grep $1)
-}
-
-# get log for the pod given
-# usage : log <namespace> <service-name>
-function log() {
-        pod=`pod $1 $2`
-        kubectl -n $1 logs -c $2 $pod -f
-}
-
-# log forwarding
-# usage : lf <namespace> <service-name>
-function lf() {
-        pod=`pod $1 $2`
-        if [[ -z "$pod" ]]; then
-                echo "no pod found in namespace $1 name $2. Searched for prefix $2-$1" >&2
-        else
-                kubectl -n $1 logs --tail=1 -c $2 -f $pod
-        fi
-}
-
-# port forwarding
-# usage : pf <namespace> <service-name> 8080:8080
-function pf() {
-        pod=`pod $1 $2`
-        if [[ -z "$pod" ]]; then
-                echo "no pod found in namespace $1 name $2" >&2
-        else
-                echo "forwarding port from $pod container $2 $3" >&2
-                kubectl -n $1 port-forward $pod $3
-        fi
-}
-
 # If you have multiple kots versions installed, using the following function
 # you can point to a version you want to use. Just run kotsv <version-number>
 
